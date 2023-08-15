31e1c027e2e4a4e4bbc35c9369ea691b626b4c2a
Tiago <tiagosoareslu4@gmail.com>
2023-08-15 11:39:21 -0300

    Atualização do projeto de exemplo

---
+++ Modelos/Equipamento.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PontoWebIntegracaoExterna.Modelos
+{
+    public class Equipamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }        
+        public string EnderecoIP { get; set; }
+    }   
+}
